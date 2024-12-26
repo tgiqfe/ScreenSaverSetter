21019c829f3ad72bbaff0528501e807e0e3c71f4
unknown <tqji31001@gmail.com>
2024-12-26 09:00:48 +0900

    Update check code.2

--- ScreenSaverSetter48/LockScreenSetting.cs
+++ ScreenSaverSetter48/LockScreenSetting.cs
@@ -8,7 +8,7 @@
         /// <param name="isLock"></param>
         /// <param name="timeout"></param>
         /// <param name="screenSaverPath"></param>
-        public static void Update(bool isLock, int timeout, string screenSaverPath)
+        public static void Update(bool? isLock, int? timeout, string screenSaverPath)
         {
             string ssPath = string.IsNullOrEmpty(screenSaverPath) ?
                 "":
